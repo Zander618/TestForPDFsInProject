824496eee2635ab3d5e0c0da0b8eaa35ac6f6b9d
Zachary Sirna <zsirna@yahoo.com>
2022-04-15 12:24:10 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from "react";
+import SinglePage from "./single-page";
+
+
+/* This is required only if the project file is located 
+inside the app. Otherwise you can use the external link of the pdf file*/
+
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Single Page</h4>
+      <SinglePage pdf="http://ia800207.us.archive.org/5/items/Cantorion_sheet_music_collection/09011231b36f8c7524f09378a273362d.pdf#track_/download/4/09011231b36f8c7524f09378a273362d/F%C3%BCr%20Elise%20Original%20version%20-%20Piano.pdf" />
+      <button>Like</button>
     </div>
   );
 }
-
-export default App;
---
+++ src/Document.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { Document, Page } from 'react-pdf';
+
+// export default function Test() {
+//   return (
+//     <Document file="http://ia800207.us.archive.org/5/items/Cantorion_sheet_music_collection/09011231b36f8c7524f09378a273362d.pdf#track_/download/4/09011231b36f8c7524f09378a273362d/F%C3%BCr%20Elise%20Original%20version%20-%20Piano.pdf">
+//       <Page pageNumber={1} />
+//     </Document>
+//   );
+// }
